c01279bdf9f2e1b10d42f993239398181d5d88ae
Trevor Bruecher <tabruecher@gmail.com>
2017-12-01 20:29:54 -0800

    added helper

---
+++ app/helper.R
@@ -0,0 +1,12 @@
+# -- helper.R ----------------------------------------------#
+#                                                           #
+# Stores helper methods to be used in the server file.      #
+# Contains the methods to be used when interacting with     #
+# dom elements on the web app itself.                       #
+#                                                           #
+# ----------------------------------------------------------#
+
+library(shiny)
+suppressPackageStartupMessages(library(dplyr))
+library(scales)
+
