48a85453e8f3a7af0192dbfd4ea1044c3bf7336d
Akash Srinagesh <akashsrinagesh@gmail.com>
2017-12-03 21:32:39 -0800

    Modified script to download images and added them to the assets folder

--- doganalysis/dogtime_data/dogtime-pics-scraper.py
+++ doganalysis/dogtime_data/dogtime-pics-scraper.py
@@ -6,7 +6,14 @@ from bs4 import BeautifulSoup
 from unidecode import unidecode
 from urllib.request import urlopen
 from urllib.request import Request
+import shutil
+import os
+from os.path import basename
 from urllib.request import urlretrieve
+from urllib.parse import urlsplit
+from urllib.parse import urlparse
+from posixpath import basename,dirname
+import ssl
 
 # Base link to all breeds listed alphabetically
 BREEDS_URL = 'http://dogtime.com/dog-breeds'
@@ -18,6 +25,15 @@ breeds_soup = BeautifulSoup(r.text, 'html.parser')
 # Build a list of all breeds in the form of <a href>dog breed</a>
 breed_blocks = breeds_soup.find_all('a', class_=['post-title'])
 breed_total = len(breed_blocks)
+
+def process_url(raw_url):
+ if ' ' not in raw_url[-1]:
+     raw_url=raw_url.replace(' ','%20')
+     return raw_url
+ elif ' ' in raw_url[-1]:
+     raw_url=raw_url[:-1]
+     raw_url=raw_url.replace(' ','%20')
+     return raw_url
 
 print('Gathering doggos... This will take a while!')
 with open('doggo-pictures.csv', 'w') as csvfile:
@@ -34,7 +50,24 @@ with open('doggo-pictures.csv', 'w') as csvfile:
         img_url = parent.find('img').get('src')
         print(img_url)
 
-        # Write to the csv
-        filewriter.writerow([breed_name, img_url])
+        # # Write to the csv
+        # filewriter.writerow([breed_name, img_url])
+
+        # Setting up downloading image
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        imgurl = process_url(img_url)
+        req = Request(imgurl, headers = {'User-Agent': 'Mozilla/5.0'})
+
+        try:
+            imgdata = urlopen(req, context=ctx).read()
+        except Exception as e:
+            print(e)
+
+        output=open("./images/" + breed_name + ".png",'wb')
+        output.write(imgdata)
+        output.close()
 
 print('DONE', end='\n\n')
